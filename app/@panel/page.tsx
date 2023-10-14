e4dd78a3d1e4127f5991f2e7b798f764d5a9d605
VlaDexa <vgrechannik@gmail.com>
2023-10-14 08:49:55 +0200

    More interactive buttons

--- app/@panel/page.tsx
+++ app/@panel/page.tsx
@@ -3,20 +3,28 @@ import { HTMLAttributes, useState } from "react"
 import Image from "next/image";
 import Pin from "../../public/pin.svg";
 import Search from "../../public/search.svg";
+import Dots from "../../public/dots.svg";
 import Button, { Type } from "../button";
 
-function SearchBar(props: HTMLAttributes<HTMLInputElement>) {
-    return <div className="px-[10px] py-1 gap-1 flex flex-row rounded-[10px] bg-[#ECECEC]">
+function SearchBar(props: HTMLAttributes<HTMLInputElement> & { onSubmit?: HTMLAttributes<HTMLFormElement>["onSubmit"] }) {
+    return <form onSubmit={(event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        props.onSubmit && props.onSubmit(event);
+    }} className="px-[10px] py-1 gap-1 flex flex-row rounded-[10px] bg-vtb-gray">
         <Image src={Pin} alt=""></Image>
-        <input {...props} className="flex-grow bg-[#ECECEC]" placeholder="Город, район, улица,..."></input>
-        <button className="ml-[10px]" aria-label="Поиск">
+        <input {...props} name="Address" className="flex-grow bg-vtb-gray" placeholder="Город, район, улица,..."></input>
+        <button type="submit" className="ml-[10px]" aria-label="Поиск">
             <Image src={Search} alt=""></Image>
         </button>
-    </div>
+    </form>
 }
 
 function More() {
-
+    return <button className="rounded-[10px] px-[23px] py-[10px] bg-vtb-gray text-[#6C6C6C] flex flex-row border-2 border-vtb-gray gap-1">
+        <span>Больше</span>
+        <Image src={Dots} alt=""></Image>
+    </button>
 }
 
 export default function Panel() {
@@ -74,7 +82,14 @@ export default function Panel() {
                 <h3>Обслуживание карт</h3>
                 <ul>
                     {cards.map((el, i) => <li key={el}>
-                        <Button styleType={cardsFlags[i] ? Type.Active : Type.Unactive}>
+                        <Button styleType={cardsFlags[i] ? Type.Active : Type.Unactive}
+                            onClick={() => {
+                                setCards((old) => {
+                                    old[i] = !old[i];
+                                    return [...old];
+                                })
+                            }}
+                        >
                             {el}
                         </Button>
                     </li>)}
@@ -83,23 +98,43 @@ export default function Panel() {
                 <ul>
                     {
                         transes.map((el, i) => <li key={el}>
-                            <Button styleType={transesFlags[i] ? Type.Active : Type.Unactive}>
+                            <Button styleType={transesFlags[i] ? Type.Active : Type.Unactive}
+                                onClick={() => {
+                                    setTranses((old) => {
+                                        old[i] = !old[i];
+                                        return [...old];
+                                    })
+                                }}
+                            >
                                 {el}
                             </Button>
                         </li>)
                     }
+                    <li>
+                        <More />
+                    </li>
                 </ul>
                 <h3>Ипотечные кредиты</h3>
                 <ul>
                     {
                         credits.map((el, i) =>
                             <li key={el}>
-                                <Button styleType={creditsFlags[i] ? Type.Active : Type.Unactive}>
+                                <Button styleType={creditsFlags[i] ? Type.Active : Type.Unactive}
+                                    onClick={() => {
+                                        setCredits((old) => {
+                                            old[i] = !old[i];
+                                            return [...old];
+                                        })
+                                    }}
+                                >
                                     {el}
                                 </Button>
                             </li>
                         )
                     }
+                    <li>
+                        <More />
+                    </li>
                 </ul>
             </li>
         </ul>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,7 @@ const config: Config = {
     colors: {
         'vtb-blue': "#1E4BD2",
         'white': "#FFFFFF",
+        'vtb-gray': "#ECECEC"
     },
     extend: {
       backgroundImage: {
