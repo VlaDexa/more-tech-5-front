365e8f71a54e35f1cab95dca7456d3dfc607452b
VlaDexa <vgrechannik@gmail.com>
2023-10-14 02:46:04 +0200

    Add Map Module

---
+++ app/Map.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { Map as YMap } from "@pbe/react-yandex-maps";
+import { useEffect, useRef, useState } from "react";
+import ymaps from "yandex-maps";
+
+export default function Map() {
+    const mapRef = useRef<ymaps.Map | undefined>(undefined);
+    const [position, setPosition] = useState([55.76, 37.64]);
+
+    useEffect(() => {
+        if (!navigator.geolocation) {
+            alert("Ваш браузер не поддерживает геолокацию")
+        } else {
+            navigator.geolocation.getCurrentPosition((position) => { setPosition([position.coords.latitude, position.coords.longitude]) }, () => { });
+        }
+    }, []);
+
+    useEffect(() => {
+        if (!mapRef.current) { return; }
+        mapRef.current.panTo(position);
+    }, [position])
+    // const ymap = useMemo(() => <YMap state={{ center: position, zoom: 9 }} width={mapRef.current?.clientWidth} height={mapRef.current?.clientHeight}></YMap>, [position, mapRef.current?.clientWidth, mapRef.current?.clientHeight]);
+
+    return <YMap defaultState={{ center: position, zoom: 9 }} className="w-full h-full p-4" instanceRef={mapRef}></YMap>;
+}
