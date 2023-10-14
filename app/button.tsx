69d7985891b114d379bc412227aa8c86c08265cc
VlaDexa <vgrechannik@gmail.com>
2023-10-15 01:24:37 +0200

    Fix them buttons

--- app/button.tsx
+++ app/button.tsx
@@ -8,7 +8,7 @@ function getClasses(type: Type): string {
     const common = "py-1 px-[23px] rounded-[10px] transition-colors border-2 motion-reduced:transition-none";
     switch (type) {
         case Type.Active: return `${common} border-vtb-blue text-white bg-vtb-blue`;
-        case Type.Unactive: return `${common} tborder-vtb-blue ext-vtb-blue hover:text-white hover:bg-vtb-blue active:bg-vtb-blue active:text-white`;
+        case Type.Unactive: return `${common} border-vtb-blue text-vtb-blue hover:text-white hover:bg-vtb-blue active:bg-vtb-blue active:text-white`;
         case Type.Disabled: return `${common} border-[#6C6C6C] text-[#6C6C6C] cursor-not-allowed`
     }
 }
